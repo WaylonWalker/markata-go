37483793438de3ac5c16fd5977da6d1ca98afd79
Waylon S. Walker <waylon@waylonwalker.com>
2026-01-24 09:34:50 -0600

    feat(plugins): Add blogroll and RSS reader plugin
    Fixes #183

--- pkg/models/config.go
+++ pkg/models/config.go
@@ -259,6 +259,9 @@ type Config struct {
 
 	// ContentTemplates configures the content template system for the new command
 	ContentTemplates ContentTemplatesConfig `json:"content_templates" yaml:"content_templates" toml:"content_templates"`
+
+	// Blogroll configures the blogroll and RSS reader functionality
+	Blogroll BlogrollConfig `json:"blogroll" yaml:"blogroll" toml:"blogroll"`
 }
 
 // HeadConfig configures elements added to the HTML <head> section.
@@ -1034,6 +1037,7 @@ func NewConfig() *Config {
 		Header:           NewHeaderLayoutConfig(),
 		FooterLayout:     NewFooterLayoutConfig(),
 		ContentTemplates: NewContentTemplatesConfig(),
+		Blogroll:         NewBlogrollConfig(),
 	}
 }
 
